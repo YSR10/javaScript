ecb6337e2825f092c8548bcb88cf6cd97170887d
sulymanyasir muhammad <sulymanyasir621@gmail.com>
2023-10-30 10:28:38 +0100

    day2

---
+++ day1/script.js
@@ -0,0 +1,63 @@
+//1 Write a single line comment which says, comments can make code readable
+
+//let firstComment = 'comments can make codes readable';
+
+//2 Write another single comment which says, Welcome to 30DaysOfJavaScript
+
+//let secondComment = 'Welcome to 30DaysOfJavascript';
+
+//3 Write a multiline comment which says, comments can make code readable, easy to reuse and informative
+
+/*let comments = 'comments 
+can make code readable, easy to reuse and informative';
+ */
+
+//4 Create a variable.js file and declare variables and assign string, boolean, undefined and null data types
+
+let sul = "sulyman";
+let isRaining = true;
+let name;
+console.log(name);
+let emptyValue = null;
+
+//5 Create datatypes.js file and use the JavaScript typeof operator to check different data types. Check the data type of each variable
+console.log(typeof `yasir`); //string
+console.log(typeof 1); //number
+console.log(typeof true); //boolean
+console.log(typeof undefined); //undefined
+console.log(typeof null); // null
+
+//6 Declare four variables without assigning values
+let firstNamee;
+let secondNamee;
+let thirdNamee;
+let fourthNamee;
+
+//7 Declare four variables with assigned values
+let one = `sulyman`;
+let two = `yasir`;
+let three = `muhammad`;
+let four = 4;
+
+//8 Declare variables to store your first name, last name, marital status, country and age in multiple lines
+let firstName = `yasir`;
+console.log(firstName);
+let lastName = `sulyman`;
+console.log(lastName);
+let maritalStatus = `single`;
+console.log(maritalStatus);
+let country = `nigeria`;
+console.log(country);
+let age = 20;
+console.log(age);
+
+//9 Declare variables to store your first name, last name, marital status, country and age in a single line
+console.log(firstName, lastName, maritalStatus, country, age);
+
+//10Declare two variables myAge and yourAge and assign them initial values and log to the browser console.
+//I am 25 years old.
+//You are 30 years old.
+let myAge = `I am 25 years old.`;
+console.log(myAge);
+
+let yourAge = `You are 30 years old.`;
---
+++ day2/script.js
@@ -0,0 +1,81 @@
+//Exercise: Level 1
+/*non-primitive data type(array & object)
+let nums = [34567];
+nums[3] = 89;
+console.log(nums);
+primitive data type(string,number, null, boolean,undifine, symbol)
+let r = `eer`;
+let t = `eer`;
+console.log(r == t);*/
+
+let challenge = `30 Days Of JavaScript`;
+console.log(challenge);
+console.log(challenge.length);
+console.log(challenge.toUpperCase());
+console.log(challenge.toLowerCase());
+console.log(challenge.substr(0, 2));
+console.log(challenge.substr(2, 19));
+console.log(challenge.includes(`Script`));
+console.log(challenge.split());
+console.log(challenge.split(``));
+
+let companies = `Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon`;
+console.log(companies.split(`,`));
+
+console.log(challenge.replace(`JavaScript`, `Python`));
+console.log(challenge.charAt(15));
+console.log(challenge.charCodeAt(`j`));
+console.log(challenge.indexOf(`a`));
+console.log(challenge.lastIndexOf(`a`));
+
+let because = `You cannot end a sentence with because because because is a conjunction`;
+console.log(because.indexOf(`because`));
+console.log(because.lastIndexOf(`because`));
+console.log(because.search(`because`));
+
+let trim = "30 Days Of JavaScript";
+console.log(trim.trim());
+console.log(trim.startsWith("30"));
+console.log(trim.endsWith("JavaScript"));
+console.log(trim.match(`a`));
+
+let con = "30 Days of";
+console.log(con.concat("JavaScript"));
+
+let re = "30 Days Of JavaScript";
+console.log(re.repeat(2));
+
+//Exercise: Level 2
+let theQuote = `There is no exercise better for the heart than reaching down and lifting people up.' by John Holmes teaches us to help one another`;
+console.log(theQuote);
+
+let motherTeresa = `Love is not patronizing and charity isn't about pity, it is about love. Charity and love are the same -- with charity you give love, so don't just give money but reach out your hand instead`;
+console.log(motherTeresa);
+
+let tenstring = `10`;
+let tennumber = 10;
+console.log(typeof `10` === 10);
+console.log(tenstring);
+
+let float = `9.8`;
+let num = 9.8;
+console.log(Math.ceil(float));
+
+let py = `python`;
+let ja = `jargon`;
+console.log(`python`.includes(`on`) && `jargon`.includes(`on`));
+
+let jargon = `I hope this course is not full of jargon`;
+console.log(`I hope this course is not full of jargon`.includes(`jargon`));
+
+let randomNum = Math.floor(Math.random() * 101);
+console.log(randomNum);
+
+let random = Math.floor(Math.random() * 50) + 50;
+console.log(random);
+
+let number = Math.floor(Math.random() * 255);
+console.log(number);
+
+let string = Math.floor(Math.random() * 10);
+console.log("JavaScript".charAt(string));
