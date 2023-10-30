589a3753db93d7cfabd59eff6746ce56985c617f
sulymanyasir muhammad <sulymanyasir621@gmail.com>
2023-10-30 12:17:02 +0100

    day2 level3

--- day2/script.js
+++ day2/script.js
@@ -93,3 +93,25 @@ console.log(`1 1 1 1 1 \n2 1 2 4 8 \n3 1 3 9 27 \n4 1 4 16 64 \n5 1 5 25 125`);
 let phraseBecause =
   "You cannot end a sentence with because because because is a conjunction";
 console.log(phraseBecause.substr(31, 23));
+
+//Exercise: level3
+let love = `Love is the best thing in this world. Some found their love and some are still looking for their love.`;
+console.log(love.match(`love`));
+
+let becausee =
+  "You cannot end a sentence with because because because is a conjunction";
+console.log(becausee.match(because));
+
+const sentence =
+  "%I $am@% a %tea@cher%, &and& I lo%#ve %te@a@ching%;. The@re $is no@th@ing; &as& mo@re rewarding as educa@ting &and& @emp%o@weri@ng peo@ple. ;I found tea@ching m%o@re interesting tha@n any ot#her %jo@bs. %Do@es thi%s mo@tiv#ate yo@u to be a tea@cher!? %Th#is 30#Days&OfJavaScript &is al@so $the $resu@lt of &love& of tea&ching";
+//   "I same a teacher, and I love teaching. There is nothing as more rewarding as educating and empowering people. I found teaching more interesting than any other jobs. Does this motivate you to be a teacher!? This 30Days&OfJavaScript is also the result of love of teaching";
+let word = sentence
+  .replace(/[^\w\s]/g, "")
+  .match(/\w+/g)
+  .reduce((acc, word) => {
+    acc[word] = (acc[word] || 0) + 1;
+    if (!(acc[word] < acc[acc.$])) acc.$ = word;
+    return acc;
+  }, {}).$;
+
+console.log(word);
