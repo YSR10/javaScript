71473c14e6a488bffab9b7a675970a120fe074f3
sulymanyasir muhammad <sulymanyasir621@gmail.com>
2023-11-01 10:36:50 +0100

    day3 level3

--- day3/script.js
+++ day3/script.js
@@ -97,26 +97,111 @@ let circumference = 2 * pi * radius;
 console.log(areaCircle);
 console.log(circumference);
 
-let x = prompt(`X`);
-let y = 2 * x - 2;
-console.log(`the slope is ${y}`);
+// 5.Calculate the slope, x - intercept and y - intercept of y = 2x - 2
+// let y_Intercept = Number(prompt('Enter Y-intercept'));
+// let x_Intercept = Number(prompt('Enter X-intercept'));
+let x_Intercept = 3;
+let y_Intercept = 4;
+let answerIntercept = (y_Intercept = 2 * x_Intercept - 2);
+console.log("Slope =", answerIntercept);
 
-let m = (10 - 2) / (6 - 2);
-console.log(m);
-console.log(y == m);
+// let x = prompt(`X`);
+// let y = 2 * x - 2;
+// console.log(`the slope is ${y}`);
 
-y = x * x + 6 * x + 9;
-x = prompt("x");
-console.log(y);
+// let y1 = prompt(`Enter y1`);
+// let x1 = prompt(`Enter x1`);
+// let y2 = prompt(`Enter y2`);
+// let x2 = prompt(`Enter x2`);
+let y1 = 2;
+let x1 = 2;
+let y2 = 6;
+let x2 = 10;
+let twoPoints = (y2 - y1) / (x2 - x1);
+console.log(twoPoints);
 
-// let name = prompt("Enter your name");
-//  let nameLenght = name.length
-//  nameLenght > 7 ? alert("your name is long");
-//  alert("your name is short");
-let name = prompt("Enter your name");
-let nameLenght = name.length;
-nameLenght > 7 ? console.log("your name is long");
-console.log("your name is short");
+//7. Compare the slope of above two questions.
+let comparism = answerIntercept >= twoPoints;
+console.log(comparism);
 
-const todaysDate = newDate();
-console.log(todaysDate);
+// 8. Calculate the value of y (y = x2 + 6x + 9). Try to use different x values and figure out at what x value y is 0.
+let x = -3;
+let calc = x ** 2 + 6 * x + 9;
+console.log(calc);
+
+// 9.Writ a script that prompt a user to enter hours and rate per hour. Calculate pay of the person?
+let hours = prompt(`Enter hours`);
+let ratePerHour = prompt(`Enter rate per hour`);
+console.log(hours * ratePerHour);
+
+// 10. If the length of your name is greater than 7 say, your name is long else say your name is short.
+let myName = "Yasir";
+if (myName.length < 7) {
+  console.log("My name is short");
+} else {
+  console.log("My name is long");
+}
+
+// 11. Compare your first name length and your family name length and you should get this output.
+let myFamilyName = `sulyman`;
+let myFirstName = `Yasir`;
+if (myFirstName.length < myFamilyName.length) {
+  console.log(
+    `my familyname, ${myFamilyName} is longer than my firstname, ${myFirstName}`
+  );
+}
+
+// 12. Declare two variables myAge and yourAge and assign them initial values and myAge and yourAge.
+// let myAge = 250
+// let yourAge = 25
+// I am 225 years older than you., this means it should be in the console
+
+let myAge = 250;
+let yourAge = 25;
+if (myAge > yourAge) {
+  console.log(`i am ${myAge - yourAge} years older than you`);
+}
+
+// 13.Using prompt get the year the user was born and if the user is 18 or above allow the user to drive if not tell the user to wait a certain amount of years.
+// Enter birth year: 1995
+// You are 25. You are old enough to drive
+// Enter birth year: 2005
+let yearOld = prompt(`how old are you`);
+if (yearOld >= 18) {
+  console.log(`you are ${yearOld}. you are old enough to drive`);
+} else {
+  console.log(
+    `you are ${yearOld}. you will be allowed to drive after ${
+      18 - yearOld
+    } years`
+  );
+}
+
+// 14. Write a script that prompt the user to enter number of years. Calculate the number of seconds a person can live. Assume some one lives just hundred years
+// Enter number of years you live: 100
+// You lived 3153600000 seconds.
+let numberOfYears = prompt(`Enter number of years you live`);
+let seconds =
+  numberOfYears * 365 * 24 * 60 * 60 + (numberOfYears / 4) * (24 * 60 * 60);
+console.log(seconds);
+
+// 15.Create a human readable time format using the Date time object
+// YYYY-MM-DD HH:mm
+// DD-MM-YYYY HH:mm
+// DD/MM/YYYY HH:mm
+
+let todayDate = new Date();
+let hour = todayDate.getHours();
+let minutes = todayDate.getMinutes();
+let day = todayDate.getDay();
+let month = todayDate.getMonth() + 1;
+let yearr = todayDate.getFullYear();
+console.log(`${yearr}-${month}-${day}-${hour}-${minutes}`);
+console.log(`${day}-${month}-${yearr}-${hour}-${minutes}`);
+console.log(`${day}/${month}/${yearr}/${hour}/${minutes}`);
+
+// Exercises :Level 3
+// Create a human readable time format using the Date time object. The hour and the minute should be all the time two digits(7 hours should be 07 and 5 minutes should be 05 )
+// YYY-MM-DD HH:mm eg. 20120-01-02 07:05
+month = month.toString().length < 2 ? `${month}` : month;
+console.log(`${yearr}-${month}-${day} ${hour}:${minutes}`);
