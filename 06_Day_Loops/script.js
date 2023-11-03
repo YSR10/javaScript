7827fdc270b12b7848eb41d51434ab1ee7be16d5
sulymanyasir muhammad <sulymanyasir621@gmail.com>
2023-11-03 13:54:40 +0100

    update:day6 level1

---
+++ 06_Day_Loops/script.js
@@ -0,0 +1,40 @@
+//1 Iterate 0 to 10 using for loop, do the same using while and do while loop
+for (let i = 0; i <= 10; i++) {
+  console.log(i);
+}
+//while loop
+let i = 0;
+while (i <= 10) {
+  console.log(i);
+  i++;
+}
+
+//do while loop
+let j = 0;
+do {
+  console.log();
+  i++;
+} while (i <= 10);
+
+//2 Iterate 10 to 0 using for loop, do the same using while and do while loop
+for (let i = 10; i >= 0; i--) {
+  console.log(i);
+}
+//while loop
+let s = 10;
+while (s >= 0) {
+  console.log(s);
+  s--;
+}
+
+//do while loop
+let t = 10;
+do {
+  console.log(t);
+  t--;
+} while (t <= 0);
+
+//3 Iterate 0 to n using for loop
+for (let i = 0; i >= n; i++) {
+  console.log(i);
+}
