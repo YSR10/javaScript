1f78de71225034374b51d7b1cb8ab000fb4a5984
sulymanyasir muhammad <sulymanyasir621@gmail.com>
2023-11-03 11:43:44 +0100

    update:day5 level2

--- 05_Day_Arrays/script.js
+++ 05_Day_Arrays/script.js
@@ -114,3 +114,43 @@ console.log(itCompanies);
 
 //23 Remove all IT companies
 console.log(itCompanies.splice());
+
+//level 2
+
+//2 First remove all the punctuations and change the string to array and count the number of words in the array
+let text = `'I love teaching and empowering people. I teach HTML, CSS, JS, React, Python.`;
+let word = text.split(`,`);
+let me = word.join(``);
+let words = me.split(`.`);
+let mee = words.join(``);
+let us = mee.split(` `);
+console.log(us);
+
+//3 In the following shopping cart add, remove, edit items
+// add 'Meat' in the beginning of your shopping cart if it has not been already added
+// add Sugar at the end of you shopping cart if it has not been already added
+// remove 'Honey' if you are allergic to honey
+// modify Tea to 'Green Tea'
+
+const shoppingCart = ["Milk", "Coffee", "Tea", "Honey"];
+// add 'Meat' in the beginning of your shopping cart if it has not been already added
+let add = shoppingCart.unshift(`Meat`);
+console.log(shoppingCart);
+// add Sugar at the end of you shopping cart if it has not been already added
+let end = shoppingCart.push(`Sugar`);
+console.log(shoppingCart);
+// remove 'Honey' if you are allergic to honey
+const shoppingCartRevmove = ["Milk", "Coffee", "Tea", "Honey"];
+let honey = shoppingCartRevmove.pop();
+console.log(honey);
+// modify Tea to 'Green Tea'
+shoppingCart[2] = `Green Tea`;
+console.log(shoppingCart);
+
+//6 Concatenate the following two variables and store it in a fullStack variable.
+const frontEnd = [`HTML`, `CSS`, `JS`, `React`, `Redux`];
+const backEnd = [`Node`, `Express`, `MongoDB`];
+const fullStack = frontEnd.concat(backEnd);
+console.log(fullStack);
+
+// level 3
