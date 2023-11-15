345de6471c884de0e708ca5b2f2dae0b94f2ea4a
sulymanyasir muhammad <sulymanyasir621@gmail.com>
2023-11-15 16:01:28 +0100

    update:day7:level1

--- 07_Day_Functions/script.js
+++ 07_Day_Functions/script.js
@@ -40,8 +40,69 @@ console.log(volumeOfRectPrism(3, 4, 5));
 
 //7 Area of a circle is calculated as follows: area = π x r x r. Write a function which calculates areaOfCircle
 
-function areaOfCircle(pi, r, r) {
-  calc = pi * r * r;
+function areaOfCircle(PI, r, r) {
+  calc = PI * r * r;
   return calc;
 }
-console.log(areaOfCircle(3.412, 9, 6));
+console.log(areaOfCircle(3.143, 9, 6));
+
+//8 Circumference of a circle is calculated as follows: circumference = 2πr. Write a function which calculates circumOfCircle
+function circumference(PI, r) {
+  let sum = 2 * PI * r;
+  return sum;
+}
+console.log(circumference(2, 3.143, 4));
+
+//9 Density of a substance is calculated as follows:density= mass/volume. Write a function which calculates density.
+function density(mass, volume) {
+  calc = mass / volume;
+  return calc;
+}
+console.log(density(5, 6));
+
+//10 Speed is calculated by dividing the total distance covered by a moving object divided by the total amount of time taken. Write a function which calculates a speed of a moving object, speed.
+function speed(distance, time) {
+  calc = distance / time;
+  return calc;
+}
+console.log(speed(10, 2));
+
+//11 Weight of a substance is calculated as follows: weight = mass x gravity. Write a function which calculates weight.
+function weight(mass, gravity) {
+  let total = mass * gravity;
+  return total;
+}
+console.log(weight(4, 6));
+
+//12 Temperature in oC can be converted to oF using this formula: oF = (oC x 9/5) + 32. Write a function which convert oC to oF convertCelsiusToFahrenheit.
+let convertCelsiusToFahrenheit = (oC) => {
+  calc = oC * (9 / 5) + 32;
+  return calc;
+};
+console.log(convertCelsiusToFahrenheit(34));
+
+//13 Body mass index(BMI) is calculated as follows: bmi = weight in Kg / (height x height) in m2. Write a function which calculates bmi. BMI is used to broadly define different weight groups in adults 20 years old or older.Check if a person is underweight, normal, overweight or obese based the information given below.
+// The same groups apply to both men and women.
+// Underweight: BMI is less than 18.5
+// Normal weight: BMI is 18.5 to 24.9
+// Overweight: BMI is 25 to 29.9
+// Obese: BMI is 30 or more
+function BMI(weight, height) {
+  let bmi = weight / height ** 2;
+
+  if (bmi < 18.5) {
+    console.log("Underweight: BMI is less than 18.5");
+  } else if (bmi >= 18.5 && bmi <= 24.9) {
+    console.log("Normal weight: BMI is 18.5 to 24.9");
+  } else if (bmi >= 25 && bmi <= 29.9) {
+    console.log("Overweight: BMI is 25 to 29.9");
+  } else if (bmi >= 30) {
+    console.log("Obese: BMI is 30 or more");
+  } else {
+    console.log(
+      "You have either made a mistake or the brower is not functioning properly"
+    );
+  }
+  return bmi;
+}
+console.log(BMI(894, 6));
